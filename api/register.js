2c2772774e6ac0c0b048276311d786d036f2ee7c
occulticplus@outlook.com <occulticplus@outlook.com>
2019-08-13 14:24:35 +0800

    implement register

--- api/register.js
+++ api/register.js
@@ -1,4 +1,6 @@
-const router = require('express').Router()
+const router = require('express').Router();
+const request = require('request');
+const Config = require('../config/basic');
 
 const { Api, JsonRpc, RpcError, Numeric } = require('eosjs');
 const { JsSignatureProvider } = require('eosjs/dist/eosjs-jssig');      // development only
@@ -6,19 +8,62 @@ const fetch = require('node-fetch');                                    // node 
 const { TextEncoder, TextDecoder } = require('util');                   // node only; native TextEncoder/Decoder
 //const { TextEncoder, TextDecoder } = require('text-encoding');          // React Native, IE11, and Edge Browsers only
 
-const privateKeys = [];
+const privateKeys = Config.privateKeyList;
 
 const signatureProvider = new JsSignatureProvider(privateKeys);
 const rpc = new JsonRpc('http://127.0.0.1:8000', { fetch });
 const api = new Api({ rpc, signatureProvider, textDecoder: new TextDecoder(), textEncoder: new TextEncoder() });
 
+const Ecc = require('eosjs-ecc');
+
 router.post('/', async (req, res) => {
     try {
         const msg = {
             name: req.body.name,
-            key: req.body.key
-        }
+            key: req.body.key,
+            walletName : req.body.walletName
+        };
         console.log('Register: name = ' + msg.name + ', Private key = ' + msg.key);
+        let walletInfo = '';
+        let options = {
+            method : 'POST',
+            url: 'http://127.0.0.1:8888/v1/wallet/create',
+            headers : { 'content-type' : 'application/json'},
+            body : msg.walletName
+        };
+        new Promise((res, rej) => {
+            request(options, (error, response, body) => {
+                if (error) throw new Error(error);
+                //walletKey = body;
+                console.log(body);
+            });
+            if(typeof(body) == 'string') {
+                /* some problems. In body is the wallet pwd. Write it out to file.*/
+                walletInfo += 'Wallet Name : ' + msg.walletName + '\n';
+                walletInfo += 'Wallet Password : ' + body + '\n';
+                res();
+            } else {
+                rej('Error: Cannot create wallet');
+            }
+        }).catch((value) => {
+            throw new Error(value);
+        }).then(() => {
+            return Ecc.randomKey();
+        }).then(privateKey => {
+            console.log('Private Key :\t', privateKey);
+            console.log('Public Key :\t', ecc.privateToPublic(privateKey));
+            walletInfo += 'Account Name : ' + msg.name + '\n';
+            walletInfo += 'Account Private Key : ' + privateKey + '\n';
+            walletInfo += 'Account Public Key : ' + ecc.privateToPublic(privateKey) + '\n';
+            options.url = 'http://127.0.0.1:8888/v1/wallet/import_key';
+            options.body = [msg.walletName, privateKey];
+            options.headers = {'content-type': 'application/x-www-form-urlencoded; charset=UTF-8'}
+            request(options, (error, response, body) => {
+                if (error) throw new Error(error);
+                console.log(body);
+            })
+        })
+
         const result = await api.transact({
             actions: [{
                 account: 'eosio',
@@ -54,13 +99,13 @@ router.post('/', async (req, res) => {
             blocksBehind: 3,
             expireSeconds: 30,
         });
-        console.log("Created a new account. Name: " + msg.name + " whose key is " + msg.key);
-        res.send("Successfully created account!");
+        console.log('Wallet Information :\n' + walletInfo);
+        res.send("Successfully created account!\n" + walletInfo);
     } catch (e) {
         console.log(e);
-        res.json({
-            'status' : 114514,
-            'message' : "Something went wrong"
+        res.render('404', {
+            status : 114514,
+            message : "Something went wrong"
         });
     }
 })
