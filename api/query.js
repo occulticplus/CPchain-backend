b03fffc9c095f7f8a126a08bb6a59f70bfeec1e5
occulticplus@outlook.com <occulticplus@outlook.com>
2019-08-19 14:48:07 +0800

    add query

--- api/query.js
+++ api/query.js
@@ -15,7 +15,46 @@ const rpc = new JsonRpc('http://127.0.0.1:8000', { fetch });
 const api = new Api({ rpc, signatureProvider, textDecoder: new TextDecoder(), textEncoder: new TextEncoder() });
 
 router.post('/', (req, res) => {
+    const msg = {
+        id: req.id,
+        name: req.name
+    }
+    try {
+        const options = {
+            method: 'POST',
+            url: 'http://127.0.0.1:5000/api/query',
+            header: {'Content-type': 'applications/json'},
+            body: JSON.stringify({name: msg.name})
+        }
 
+        request(options, (error, response, body) => {
+            if (error) {
+                console.log(error);
+                throw new Error('Cannot connect to smart server!');
+            }
+            const result = JSON.parse(body);
+            console.log(msg.name + '\'s transaction record: ')
+            result.forEach((r, i) => {
+                console.log("the " + i + "th record:");
+                console.log({
+                    id: r.id,
+                    hash: r.hash
+                })
+            });
+
+            res.send({
+                status: 200,
+                message: 'successfully got the user\'s tranaction list.',
+                data: body
+            }).end();
+        })
+    } catch (e) {
+        console.log(e);
+        res.send({
+            status: 500,
+            message: 'Cannot Excecute operations. Something went wrong.'
+        })
+    }
 })
 
 module.exports = router
